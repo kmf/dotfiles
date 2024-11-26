e3890b4f4811c77987b6c23f8698807ebd3c4075
Karl Fischer <kmf@kmf.co>
2024-11-26 13:20:28 +0200

    fix(nixvim): Fix ActivityWater Plugin

---
+++ nixos/modules/nixvim/plugins/extraPlugins.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+
+let
+  buildVimPlugin =
+    {
+      name,
+      owner,
+      rev,
+      sha256,
+    }:
+    pkgs.vimUtils.buildVimPlugin rec {
+      inherit name;
+      src = pkgs.fetchFromGitHub {
+        inherit owner rev sha256;
+        repo = name;
+      };
+    };
+in
+{
+  programs.nixvim = {
+  extraPlugins = [
+    (buildVimPlugin {
+      name = "aw-watcher-vim";
+      owner = "ActivityWatch";
+      rev = "4ba86d05a940574000c33f280fd7f6eccc284331";
+      sha256 = "I7YYvQupeQxWr2HEpvba5n91+jYvJrcWZhQg+5rI908=";
+    })
+  ];
+  };
+}
