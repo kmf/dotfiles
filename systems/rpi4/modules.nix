3fa7e24167d978ce846856aa2d34d8b4fd729ef7
Karl Fischer <kmf@kmf.co>
2024-12-19 07:25:05 +0200

    feat(modules): Added home-assistant

--- systems/rpi4/modules.nix
+++ systems/rpi4/modules.nix
@@ -2,6 +2,7 @@
   imports = [
     ../../nixos/modules/docker.nix
     ../../nixos/modules/env.nix
+    ../../nixos/modules/home-assistant.nix
     ../../nixos/modules/network.nix
     ../../nixos/modules/nixvim/nixvim.nix
     ../../nixos/modules/podman.nix
