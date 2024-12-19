63a55b2a2533dacbed81db6e6db37afd3ac82ef5
Karl Fischer <kmf@kmf.co>
2024-12-19 08:12:17 +0200

    fix(home-assistant): Refactor Config

--- nixos/modules/home-assistant.nix
+++ nixos/modules/home-assistant.nix
@@ -1,17 +1,36 @@
 {
-    virtualisation.oci-containers = {
-    backend = "podman";
-    containers.homeassistant = {
-      volumes = [ "home-assistant:/config" ];
-      environment.TZ = "Africa/Johannesburg";
-      # Note: The image will not be updated on rebuilds, unless the version label changes
-      image = "ghcr.io/home-assistant/home-assistant:stable";
-      extraOptions = [ 
-        # Use the host network namespace for all sockets
-        "--network=host"
-        # Pass devices into the container, so Home Assistant can discover and make use of them
-        "--device=/dev/ttyACM0:/dev/ttyACM0"
-      ];
+  services.home-assistant = {
+    enable = true;
+        openFirewall = true;
+
+    extraComponents = [
+      # Components required to complete the onboarding
+      "analytics"
+      "apple_tv"
+      "cups"
+      "default_config"
+      "enegry"
+      "google_translate"
+      "ipp"
+      "met"
+      "radio_browser"
+      "shopping_list"
+      "snmp"
+      "tplink"
+      "upnp"
+
+      # Recommended for fast zlib compression
+      # https://www.home-assistant.io/integrations/isal
+      "isal"
+    ];
+    config = {
+      # Includes dependencies for a basic setup
+      # https://www.home-assistant.io/integrations/default_config/
+      default_config = {};
+      homeassistant.time_zone = "Africa/Johannesburg";
+      homeassistant.unit_system = "metric";
+      homeassistant.temperature_unit = "C";
+      homeassistant.name = "rpi4";
     };
   };
 }
