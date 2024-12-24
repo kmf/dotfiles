39bfce42fa20b301c528980566d5804da73d8d58
Karl Fischer <kmf@kmf.co>
2024-12-24 05:56:30 +0200

    feat(hyprland): Added a Hyprland NixOS config

--- nixos/modules/hyprland.nix
+++ nixos/modules/hyprland.nix
@@ -1,9 +1,18 @@
 {
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.videoDrivers = [ "nvidia" ];
+
   programs.hyprland = {
     enable = true;
+    withUWSM = true;
     xwayland.enable = true;
   };
   programs.hyprlock.enable = true;
   services.hypridle.enable = true;
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  environment.sessionVariables = {
+    WL_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+  xdg.portal.enable = true;
 }
