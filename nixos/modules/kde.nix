4a707fc9ca72f4cc7fb0088a3c1cac42db52bbfc
Karl Fischer <kmf@kmf.co>
2024-12-28 07:57:00 +0200

    fix(kde): Screenlocker Bugs and Virtualkeyboard

--- nixos/modules/kde.nix
+++ nixos/modules/kde.nix
@@ -2,7 +2,13 @@
   services.xserver.enable = true; # optional
   services.xserver.videoDrivers = [ "nvidia" ];
   services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.settings = {
+    General = {
+      InputMethod="";
+    };
+  };
   services.displayManager.sddm.wayland.enable = false;
   services.displayManager.defaultSession = "plasmax11";
   services.desktopManager.plasma6.enable = true;
+  services.desktopManager.plasma6.enableQt5Integration = true;
 }
