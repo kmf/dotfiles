3f6d413270d07e1d654b09b5c2f2acddfac37b36
Karl Fischer <kmf@kmf.co>
2025-01-02 08:20:13 +0200

    feat(zed): Better Config Settings

--- home-manager/modules/zed-editor.nix
+++ home-manager/modules/zed-editor.nix
@@ -1,4 +1,11 @@
-{  programs.zed-editor = {
+{
+  programs.zed-editor = {
     enable = true;
+    userSettings = {
+        ui_font_size = 18;
+        ui_font_family = "CodeNewRoman Nerd Font";
+        buffer_font_size = 18;
+        buffer_font_family = "CodeNewRoman Nerd Font";
+    };
   };
 }
