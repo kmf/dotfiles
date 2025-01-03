872a2910e4a92037070b6dbd4f165924169b47b7
Karl Fischer <kmf@kmf.co>
2025-01-03 14:28:28 +0200

    feat(flox): Added Flox

--- systems/oakenshield/configuration.nix
+++ systems/oakenshield/configuration.nix
@@ -9,6 +9,8 @@
 #    ./modules/xserver.nix
 #  ];
 
+  nix.settings.trusted-substituters = [ "https://cache.flox.dev" ];
+  nix.settings.trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
 
   networking.hostName = "oakenshield"; # Define your hostname.
 
