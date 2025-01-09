07af5538016f8011ea67a294554e07310c0df96f
Karl Fischer <kmf@kmf.co>
2025-01-09 10:14:39 +0200

    fix(samba): Fixes for nmbd

--- nixos/modules/samba.nix
+++ nixos/modules/samba.nix
@@ -2,15 +2,13 @@
   services.samba = {
   enable = true;
   openFirewall = true;
+  nmbd.enable = true;
   settings = {
     global = {
     "workgroup" = "GreatDane";
     "server string" = "oakenshield";
     "netbios name" = "oakenshield";
     "security" = "user";
-    #use sendfile = yes
-    #max protocol = smb2
-    # note: localhost is the ipv6 localhost ::1
     "hosts allow" = "192.168.0. 127.0.0.1 localhost";
     "hosts deny" = "0.0.0.0/0";
     "guest account" = "nobody";
